a7970c0afd77eeb43db2527473ffe2b120c78fc9
Leandro Bandeira <leoviolinist22@gmail.com>
2025-05-21 20:50:36 -0300

    adding new problem

--- LeetCode150/romanToInteger/main.cpp
+++ LeetCode150/romanToInteger/main.cpp
@@ -14,13 +14,43 @@ public:
             {'D', 500},
             {'M', 1000}
         };
+      map<char, int> romansCount = {
+            {'I', 0},
+            {'V', 0},
+            {'X', 0},
+            {'L', 0},
+            {'C', 0},
+            {'D', 0},
+            {'M', 0}
+        };
       
       int value = 0;
-      for(int i = s.size() - 1; i >= 0; i--){
-        cout << romansValue[s[i]] << '\n';
-        value += romansValue[s[i]];
-      }
+      bool sum = 1;
+      
+      int i = s.size() - 1;
+      while(i >= 0){
+        if(i < s.size() - 1 && s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X')){
+          sum = 0;
+          
+        }        
+        if(i < s.size() - 1 && s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C')){
+          sum = 0;
+          
+        }
 
+        if(i < s.size() - 1 && s[i] == 'C' && (s[i + 1] == 'D' || s[i + 1] == 'M')){
+          sum = 0;
+          
+        }
+        
+        if(sum){
+          value += romansValue[s[i]];
+        }else{
+          value -= romansValue[s[i]];
+        }
+        sum = 1;
+        i--;
+      }
       return value;    
     }
 };
